1843c103abad05edb5a281173f9e7b292148e62e
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-05 19:17:47 -0300

    fix: home route

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,5 +1,13 @@
+import { Navbar } from "../../components/Header/Navbar";
+
 export function Home() {
     return (
-        <div>HOME</div>
+        <>
+            <div className="bg-[#343434] w-full h-[1000px]">
+                <Navbar
+                    items={['Characters', 'Clans', 'Akatsuki', 'Tailed Beasts']}
+                />
+            </div>
+        </>
     )
 }
