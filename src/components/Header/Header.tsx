543b04bebd9a66930e2577b35c1601c41944bb61
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-07 20:45:22 -0300

    feat: add componentes in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,13 @@
+import { Navbar } from "./Navbar";
+import banner from '../../assets/naruto-chibi.png'
+
 export function Header() {
     return (
-        <div></div>
+        <header>
+            <Navbar
+                items={['Characters', 'Clans', 'Akatsuki', 'Tailed Beasts']}
+            />
+            <img src={banner} alt="" className="w-full h-[155px]" />
+        </header>
     )
 }
