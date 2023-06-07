c93c9657fd47fa101572839b5cd3e2712b4ec0ee
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-07 20:58:11 -0300

    feat: add footer components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
+
 export function Footer() {
     return (
-        <footer>
-
+        <footer className="h-[90px] flex justify-center items-center gap-[14px]">
+            <p className="font-MPLUS1CODE text-[16p] text-white">by miqstelles</p>
+            <a href="https://github.com/miqstelles" target="_blank"><GithubLogo size={32} color="#fff" /></a>
+            <a href="https://linkedin.com/in/miquéiastelles" target="_blank"><LinkedinLogo size={32} color="#fff" /></a>
         </footer>
     )
 }
