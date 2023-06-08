94eab71387dc6a942b9211f95a822a73e8ea5cdc
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-08 20:59:04 -0300

    feat: add characters page route

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/home";
+import { Characters } from "../pages/Characters/characters";
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/Characters" element={<Characters />} />
             </Routes>
         </Router>
     )
