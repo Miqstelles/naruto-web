e25e5d005ecef10c14a0f397558882655fde4da2
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-07 23:27:28 -0300

    fix: unnecessary key prop

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,20 +1,16 @@
-interface index {
-    key: number;
-}
-
-export function Skeleton(props: index) {
+export function Skeleton() {
     return (
-        <div role="status" key={props.key} className="w-[330px] h-[219px] rounded-[5px] dark:bg-[#b87f65] animate-pulse transition-opacity duration-500">
+        <div role="status" className="w-[330px] h-[219px] rounded-[5px] dark:bg-[#b87f65] animate-pulse transition-opacity duration-500">
             <div className="w-[330px] h-[132px] dark:bg-[#e9a07f] rounded-t-[5px]"></div>
-            <div className="grid gap-[20px] justify-center">
-                <div className="flex gap-[20px]">
-                    <div className="w-[50px] h-[12px] dark:bg-[#b45f37]"></div>
-                    <div className="w-[50px] h-[12px] dark:bg-[#b45f37]"></div>
+            <div className="grid gap-[20px] mt-[10px] mx-[14px]">
+                <div className="flex justify-between gap-[20px]">
+                    <div className="w-[140px] h-[20px] rounded-[5px] dark:bg-[#b45f37]"></div>
+                    <div className="w-[90px] h-[20px] rounded-[5px] dark:bg-[#b45f37]"></div>
                 </div>
 
-                <div className="flex gap-[20px]">
-                    <div className="w-[50px] h-[12px] dark:bg-[#b45f37]"></div>
-                    <div className="w-[50px] h-[12px] dark:bg-[#b45f37]"></div>
+                <div className="flex justify-between gap-[20px]">
+                    <div className="w-[120px] h-[15px] rounded-[5px] dark:bg-[#b45f37]"></div>
+                    <div className="w-[80px] h-[15px] rounded-[5px] dark:bg-[#b45f37]"></div>
                 </div>
             </div>
         </div>
