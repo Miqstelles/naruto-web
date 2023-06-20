20d595a67be9af888f154470d1aa416a10385455
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-19 23:22:11 -0300

    feat: add api connection to get character using its id

--- src/pages/CharacterDetails/characterDetails.tsx
+++ src/pages/CharacterDetails/characterDetails.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Character, fetchCharacter } from "../../services/api";
 
 export function CharacterDetails() {
     const { id } = useParams<{ id: string }>()
+    const [character, setCharacter] = useState<Character[]>([])
+
+    useEffect(() => {
+        fetchCharacter(`?page=${Number(id) + 1}&limit=1`)
+            .then(response => setCharacter(response.data.characters))
+            .catch(error => console.log(error))
+    })
 
     return (
-        <div>{id}</div>
+        <div className="w-full h-full">
+            {character && character.map(character => {
+                return (
+                    <p>{character.name}</p>
+                )
+            })
+            }
+        </div>
     )
 }
