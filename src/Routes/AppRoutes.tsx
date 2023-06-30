2b774793be087465f19a91a72b1ec41c53efbe08
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-06-29 22:15:57 -0300

    feat: create clans page and route

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/home";
 import { Characters } from "../pages/Characters/characters";
 import { CharacterDetails } from "../pages/CharacterDetails/characterDetails";
+import { Clans } from "../pages/Clans/clans";
 
 const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/Characters/:afiliaton?" element={<Characters />} />
                 <Route path="/Character/:id" element={<CharacterDetails />} />
+                <Route path="Clans" element={<Clans />} />
             </Routes>
         </Router>
     )
---
+++ src/pages/Clans/clans.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "../../components/Footer";
+import { Navbar } from "../../components/Header/Navbar";
+
+export function Clans() {
+    return (
+        <div className="bg-[#343434] w-full h-full">
+            <Navbar
+                items={['Characters', 'Clans', 'Akatsuki', 'Tailed Beasts']}
+            />
+
+            <main>
+
+            </main>
+
+            <Footer />
+        </div >
+    )
+}
